a095db936b49d134fe19f05df31e83f3c10b500e
박영서 <24scarlett10@sookmyung.ac.kr>
2026-08-13 12:55:57 +0900

    feat: splashPage 제작

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -1,108 +1,36 @@
-import styled, { keyframes } from "styled-components";
 import logo from "../assets/logo_SplashPage.svg";
+import orangeSplash from "../assets/orange_SplashPage.svg";
+
+import {
+  Container,
+  LogoGroup,
+  LogoArea,
+  Logo,
+  Orbit,
+  OrangeCircle,
+  BrandName,
+} from "../styles/SplashPage.styles";
 
 const SplashPage = () => {
   return (
     <Container>
-      <LogoArea>
-        <Logo src={logo} alt="SKINEARTH logo" />
+      <LogoGroup>
+        <LogoArea>
+          <Logo src={logo} alt="SKINEARTH logo" />
 
-        <Orbit>
-          <OrangeDot />
-        </Orbit>
-      </LogoArea>
+          <Orbit>
+            <OrangeCircle
+              src={orangeSplash}
+              alt=""
+              aria-hidden="true"
+            />
+          </Orbit>
+        </LogoArea>
 
-      <BrandName>SKINEARTH</BrandName>
+        <BrandName>SKINEARTH</BrandName>
+      </LogoGroup>
     </Container>
   );
 };
 
 export default SplashPage;
-
-const orbit = keyframes`
-  from {
-    transform: translate(-50%, -50%) rotate(0deg);
-  }
-
-  to {
-    transform: translate(-50%, -50%) rotate(540deg);
-  }
-`;
-
-const Container = styled.main`
-  width: 100%;
-  min-height: 100dvh;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  background: linear-gradient(
-    180deg,
-    #09152a 0%,
-    #142c55 48%,
-    #284f8d 100%
-  );
-
-  overflow: hidden;
-`;
-
-const LogoArea = styled.div`
-  position: relative;
-
-  width: 124px;
-  height: 124px;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Logo = styled.img`
-  width: 100%;
-  height: 100%;
-  display: block;
-`;
-
-const Orbit = styled.div`
-  position: absolute;
-
-  top: 50%;
-  left: 50%;
-
-  width: 124px;
-  height: 124px;
-
-  animation: ${orbit} 2.2s cubic-bezier(0.45, 0, 0.55, 1) forwards;
-`;
-
-const OrangeDot = styled.div`
-  position: absolute;
-
-  top: -10px;
-  left: 50%;
-
-  width: 20px;
-  height: 20px;
-
-  transform: translateX(-50%);
-
-  border-radius: 50%;
-  background: #ff664b;
-
-  box-shadow: 0 0 16px rgba(255, 102, 75, 0.2);
-`;
-
-const BrandName = styled.h1`
-  margin-top: 32px;
-
-  font-family: "EstablishRetrosans", sans-serif;
-  font-size: clamp(42px, 12vw, 58px);
-  font-weight: normal;
-
-  line-height: 1;
-  letter-spacing: 1px;
-
-  color: #f6f7fa;
-`;
---
+++ src/styles/SplashPage.styles.js
@@ -0,0 +1,84 @@
+import styled, { keyframes } from "styled-components";
+
+export const orbit = keyframes`
+  from {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+
+  to {
+    transform: translate(-50%, -50%) rotate(540deg);
+  }
+`;
+
+export const Container = styled.main`
+  width: 100%;
+  min-height: 100dvh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: linear-gradient(
+    171deg,
+    var(--dark-navy) 1.51%,
+    var(--navy) 97.76%
+  );
+
+  overflow: hidden;
+`;
+
+export const LogoGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const LogoArea = styled.div`
+  position: relative;
+  width: 124px;
+  height: 124px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Logo = styled.img`
+  width: 100%;
+  height: 100%;
+  display: block;
+`;
+
+export const Orbit = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  width: 88px;
+  height: 88px;
+
+  animation: ${orbit} 2.2s cubic-bezier(0.45, 0, 0.55, 1) forwards;
+`;
+
+export const OrangeCircle = styled.img`
+  position: absolute;
+  top: 0;
+  left: 50%;
+
+  width: 20px;
+  height: 20px;
+
+  transform: translate(-50%, -50%);
+  display: block;
+`;
+
+export const BrandName = styled.h1`
+  margin-top: 32px;
+
+  font-family: "EstablishRetrosans", sans-serif;
+  font-size: 32px;
+  font-weight: normal;
+  line-height: 1;
+
+  color: var(--white);
+`;

c1dbff853c6d21acbd8828e14b662fb3ebb8c06d
24yp01 <24scarlett10@sookmyung.ac.kr>
2026-08-15 00:57:24 +0900

    feat: router:

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import SplashPage from "./pages/SplashPage";
 import OnboardingPage from "./pages/OnboardingPage";
+import HomePage from "./pages/HomePage";
+import LogPage from "./pages/LogPage";
+import MissionPage from "./pages/MissionPage";
+import MyPage from "./pages/MyPage";
+import PredictionPage from "./pages/PredictionPage";
 
 function App() {
   const [showSplash, setShowSplash] = useState(true);
 
+  if (showSplash) {
+    return (
+      <SplashPage
+        onFinish={() => setShowSplash(false)}
+      />
+    );
+  }
+
   return (
-    <>
-      {showSplash ? (
-        <SplashPage
-          onFinish={() => setShowSplash(false)}
-        />
-      ) : (
-        <OnboardingPage />
-      )}
-    </>
+    <Routes>
+      <Route path="/" element={<OnboardingPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/log" element={<LogPage />} />
+      <Route path="/prediction" element={<PredictionPage />} />
+      <Route path="/mission" element={<MissionPage />} />
+      <Route path="/my" element={<MyPage />} />
+    </Routes>
   );
 }
 

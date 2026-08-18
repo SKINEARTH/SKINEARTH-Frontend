bf628d4daca24ca71a739917b901cefadc51ca4f
박영서 <24scarlett10@sookmyung.ac.kr>
2026-08-18 23:11:12 +0900

    feat: 마이페이지 개인정보 편집 기능 추가

--- src/api/myPage.js
+++ src/api/myPage.js
@@ -3,3 +3,22 @@ import { apiRequest } from "./apiClient";
 export const getMyPage = () => {
   return apiRequest("/api/users/me");
 };
+
+export const updateMyPage = ({
+  nickname,
+  userStatus,
+  skinConcerns,
+}) => {
+  return apiRequest(
+    "/api/users/me/personalization",
+    {
+      method: "PUT",
+      body: JSON.stringify({
+        nickname,
+        userStatus,
+        skinConcerns,
+        skinConcernSelectionUnique: true,
+      }),
+    }
+  );
+};
